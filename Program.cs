2a45d9eb67957771b8946839d0b8da9360da9762
nvhung18i <48077769+nvhung18i@users.noreply.github.com>
2019-03-22 13:59:30 +0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,26 +4,53 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace bai2
+namespace sinhvien
 {
     class Program
     {
         static void Main(string[] args)
         {
             int n;
-            Console.WriteLine("nhap so doan tau:");
+            Console.WriteLine("nhap so sv:");
             n = Int32.Parse(Console.ReadLine());
-            doantau[] mangdt = new doantau[100];
-            for (int i=0;i<n;i++)
+            sinhvien[] sv = new sinhvien[n];
+            for (int i = 0; i < n; i++)
             {
-                mangdt[i] = new doantau();
-                mangdt[i].nhap();
-                mangdt[i].intt();
+                sv[i] = new sinhvien();
+                sv[i].nhap();
+                sv[i].IN();
+            }
+            float min = sv[0].tinhdtb();
+            for (int i = 1; i < n; i++)
+            {
+                if (sv[i].tinhdtb() < min)
+                    min = sv[i].tinhdtb();
+            }
+            Console.WriteLine("sv co diem thap nhat la{0}" ,min);
+
+            for (int j = 0; j < n; j++)
+            {
+                for (int x = j + 1; x < n; x++)
+                {
+                    if (sv[x].tinhdtb() < sv[j].tinhdtb())
+                    {
+                        sinhvien sx = new sinhvien();
+                        sx = sv[j];
+                        sv[j] = sv[x];
+                        sv[x] = sx;
+                    }
+                }
+            }
+            Console.WriteLine("  sinh vien sau khi xap sep tang dan la:");
+            for (int j = 0; j < n; j++)
+                Console.WriteLine("sinh vien ma {0} co diem TB la : {1}", sv[j].masv, sv[j].tinhdtb());
+
+            for (int i = 0; i < n; i++)
+            {
+                if (sv[i].masv.Contains("IT"))
+                    Console.WriteLine("sinh vien ma {0} co diem TB la:{1}", sv[i].masv, sv[i].tinhdtb());
 
             }
-            doantau tau1 = new doantau();
-            tau1.nhap();
-            tau1.intt();
             Console.ReadLine();
         }
     }
---
+++ tinhdiemtrungbinh.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sinhvien
+{
+    class sinhvien
+    {
+        public String masv;
+        float toan;
+        float ly;
+        float hoa;
+        //default constructors
+        public sinhvien()
+        {
+            this.masv = "01";
+            this.toan = (float)7.5;
+            this.ly = (float)8.5;
+            this.hoa = (float)6.0;
+        }
+        public sinhvien(String masv, float toan, float ly, float hoa)
+        {
+            this.masv = masv;
+            this.toan = toan;
+            this.ly = ly;
+            this.hoa = hoa;
+        }
+        public void nhap()
+        {
+            Console.WriteLine("nhap masv:");
+            this.masv =Console.ReadLine();
+            Console.WriteLine("nhap diem toan:");
+            this.toan = float.Parse(Console.ReadLine());
+            Console.WriteLine("nhap diem ly:");
+            this.ly = float.Parse(Console.ReadLine());
+            Console.WriteLine("nhap diem hoa:");
+            this.hoa = float.Parse(Console.ReadLine());
+        }
+        public float tinhdtb()
+        {
+            return (float)(this.toan + this.ly + this.hoa) / 3;
+        }
+        public void IN()
+            {
+              Console.WriteLine("masv {0} co dtb la {1}",this.masv,this.tinhdtb());
+           
+
+
+            Console.ReadLine();
+        }
+    }
+}
